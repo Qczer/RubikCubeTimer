1fc0858edd568fd8e0f7c528ccaaf1cef6c92ad3
Qczer <119138392+Qczer@users.noreply.github.com>
2026-07-06 00:00:46 +0200

    feat: Start Stats page

--- app/pages/solves.vue
+++ app/pages/solves.vue
@@ -1,1 +1,19 @@
-<template>Solves</template>
+<script setup lang="ts">
+import Button from '~/components/Solves/button.vue'
+</script>
+
+<template>
+  <div class="flex flex-1 flex-col gap-4 p-8">
+    <Header title="Solves" />
+    <div class="flex gap-2 p-2">
+      <div>
+        <div class="flex gap-2">
+          <Button text="3x3" icon="cube" />
+          <Button icon="filter" />
+          <Button icon="sort" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
